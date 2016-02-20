cda545ba3656faec283c6b8a3a60b13456825454
Chris Hitchcott <hitchcott@gmail.com>
2016-02-20 20:34:52 +0000

    fix dapple

---
+++ examples/5 Dapple/README.md
@@ -0,0 +1,21 @@
+# Dapple Example
+
+## Installation
+
+```bash
+# use node 5
+nvm install 5
+nvm use 5
+# install dapple
+npm install -g dapple
+# install the dapple packages
+dapple install
+```
+
+## Usage
+
+Test the contracts
+
+```bash
+dapple test
+```
---
+++ examples/5 Dapple/contracts/sol/test/token_test.sol
@@ -0,0 +1,14 @@
+import 'dapple/test.sol';
+import 'dappsys/token/token_test.sol';
+import 'mytoken.sol';
+
+contract MyTokenTest is Test {
+    function testEIP20() {
+      var token = new MyToken(100);
+      assertEq(token.transferred(), 0); // start at 0
+
+      var tester = new DSTokenTest();
+      token.transfer(address(tester), 10);
+      assertEq(token.transferred(), 10, "Didn't register trasnfer.");
+    }
+}
